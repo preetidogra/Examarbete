e31b720ac9293efbce8dc906bd7e18020781017a
preetidogra <70689452+preetidogra@users.noreply.github.com>
2021-12-14 10:06:12 +0100

    first commit

---
+++ src/components/Services.vue
@@ -0,0 +1,24 @@
+<template>
+    <h3> Our Services </h3>
+  
+      <div class="card">
+        <div class="card-image"></div>
+        <div class="card-text"></div>
+        <h5>Industries</h5>
+        <li>Healthcare</li>
+        <li>E-Commerce</li>
+        <li>Retail</li>
+        <li>Entertainment</li>
+        <li>Tourism</li>
+      </div>
+
+</template>
+
+<script>
+
+export default {
+    name:"Services"
+
+}
+
+</script>
